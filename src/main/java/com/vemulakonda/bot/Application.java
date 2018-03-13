887321cc027719305a15a8046de0e2b71442a7c6
pradeepvemulakonda@gmail.com <ramani456%>
2018-03-13 19:07:20 +1100

    initia

--- src/main/java/com/vemulakonda/bot/Application.java
+++ src/main/java/com/vemulakonda/bot/Application.java
@@ -1,5 +1,8 @@
 package com.vemulakonda.bot;
 
+import com.ibm.watson.developer_cloud.conversation.v1.Conversation;
+import com.ibm.watson.developer_cloud.conversation.v1.model.ListWorkspacesOptions;
+import com.ibm.watson.developer_cloud.conversation.v1.model.WorkspaceCollection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
@@ -9,10 +12,16 @@ import org.springframework.stereotype.*;
 @SpringBootApplication
 public class Application {
 
-    @RequestMapping("/")
+    @RequestMapping("/message")
     @ResponseBody
-    String home() {
-        return "Hello World!";
+    String chat() {
+        Conversation service = new Conversation("2018-02-16");
+        service.setUsernameAndPassword("4f70690b-f5b5-4bce-874d-2480678b4e4b", "7Qc3Ycxodzdc");
+        ListWorkspacesOptions options = new ListWorkspacesOptions.Builder().build();
+
+        WorkspaceCollection workspaces = service.listWorkspaces(options).execute();
+
+        return workspaces.getWorkspaces().get(0).getName();
     }
 
     public static void main(String[] args) {
