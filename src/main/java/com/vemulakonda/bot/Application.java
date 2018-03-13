aadbd8f7ad36c32a0b8128ed9a72fba8dac05fbb
pradeepvemulakonda@gmail.com <ramani456%>
2018-03-13 19:17:31 +1100

    change

--- src/main/java/com/vemulakonda/bot/Application.java
+++ src/main/java/com/vemulakonda/bot/Application.java
@@ -1,8 +1,7 @@
 package com.vemulakonda.bot;
 
 import com.ibm.watson.developer_cloud.conversation.v1.Conversation;
-import com.ibm.watson.developer_cloud.conversation.v1.model.ListWorkspacesOptions;
-import com.ibm.watson.developer_cloud.conversation.v1.model.WorkspaceCollection;
+import com.ibm.watson.developer_cloud.conversation.v1.model.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
@@ -17,11 +16,18 @@ public class Application {
     String chat() {
         Conversation service = new Conversation("2018-02-16");
         service.setUsernameAndPassword("4f70690b-f5b5-4bce-874d-2480678b4e4b", "7Qc3Ycxodzdc");
-        ListWorkspacesOptions options = new ListWorkspacesOptions.Builder().build();
 
-        WorkspaceCollection workspaces = service.listWorkspaces(options).execute();
+        String workspaceId = "ae478b9a-7a27-4cd7-83d6-cabdefc71393";
 
-        return workspaces.getWorkspaces().get(0).getName();
+        InputData input = new InputData.Builder("Hello").build();
+
+        MessageOptions options = new MessageOptions.Builder(workspaceId)
+                .input(input)
+                .build();
+
+        MessageResponse response = service.message(options).execute();
+
+        return response.getOutput().getText().get(0).toString();
     }
 
     public static void main(String[] args) {
