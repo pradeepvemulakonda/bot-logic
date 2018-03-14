1dae89c25ac32e1fee167e8bbb3c4d390f73f6db
Pradeep Vemulakonda <pradeepvemulakonda@gmail.com>
2018-03-14 14:27:00 +1100

    Added environment variables

--- src/main/java/com/vemulakonda/bot/Application.java
+++ src/main/java/com/vemulakonda/bot/Application.java
@@ -1,24 +1,37 @@
 package com.vemulakonda.bot;
 
 import com.ibm.watson.developer_cloud.conversation.v1.Conversation;
-import com.ibm.watson.developer_cloud.conversation.v1.model.*;
+import com.ibm.watson.developer_cloud.conversation.v1.model.InputData;
+import com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions;
+import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Value;
 
 @Controller
 @SpringBootApplication
 public class Application {
+
+    @Value("${UN}")
+    private String userName;
+
+    @Value("${PWD}")
+    private String password;
+
+    @Value("${WID}")
+    private String workspaceId;
 
     @RequestMapping("/message")
     @ResponseBody
     @PostMapping
     String chat(@RequestBody String inputData) {
         Conversation service = new Conversation("2018-02-16");
-        service.setUsernameAndPassword("4f70690b-f5b5-4bce-874d-2480678b4e4b", "7Qc3Ycxodzdc");
-
-        String workspaceId = "ae478b9a-7a27-4cd7-83d6-cabdefc71393";
+        service.setUsernameAndPassword(userName, password);
 
         InputData input = new InputData.Builder(inputData).build();
 
@@ -28,7 +41,7 @@ public class Application {
 
         MessageResponse response = service.message(options).execute();
 
-        return response.getOutput().getText().get(0).toString();
+        return response.getOutput().getText().get(0);
     }
 
     public static void main(String[] args) {
