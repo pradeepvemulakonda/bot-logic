1c30afb5c5d2b4abb5ed93be7e7580ca22e6008b
Pradeep Vemulakonda <pradeepvemulakonda@gmail.com>
2018-03-15 16:14:56 +1100

    comment

--- src/main/java/com/vemulakonda/bot/Application.java
+++ src/main/java/com/vemulakonda/bot/Application.java
@@ -6,11 +6,10 @@ import com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Value;
 
 @Controller
@@ -26,10 +25,18 @@ public class Application {
     @Value("${WID}")
     private String workspaceId;
 
+    @Value("${TOKEN}")
+    private String token;
+
     @RequestMapping("/message")
     @ResponseBody
     @PostMapping
-    String chat(@RequestBody String inputData) {
+    String chat(@RequestBody InputText inputData) {
+
+        if(!token.equalsIgnoreCase(inputData.getToken())) {
+            throw new IllegalStateException();
+        }
+
         System.out.println("----->>>>u:" + userName);
         System.out.println("----->>>>p:" + pwd);
         System.out.println("----->>>>w:" + workspaceId);
@@ -37,7 +44,7 @@ public class Application {
         Conversation service = new Conversation("2018-02-16");
         service.setUsernameAndPassword(userName, pwd);
 
-        InputData input = new InputData.Builder(inputData).build();
+        InputData input = new InputData.Builder(inputData.getMessage()).build();
 
         MessageOptions options = new MessageOptions.Builder(workspaceId)
                 .input(input)
@@ -46,6 +53,11 @@ public class Application {
         MessageResponse response = service.message(options).execute();
 
         return response.getOutput().getText().get(0);
+    }
+
+    @ExceptionHandler
+    ResponseEntity handle(Exception e) {
+        return new ResponseEntity("Opps, I need to go to the loo. Be right back.", HttpStatus.BAD_REQUEST);
     }
 
     public static void main(String[] args) {
